2d98d51c768b26723d789c42cb530454acbc4ef1
leahcimto <68165163+leahcimto@users.noreply.github.com>
2021-04-25 08:32:50 -0400

    Update and rename down.html to 503.php

--- 503.php
+++ 503.php
@@ -1,3 +1,9 @@
+<?php
+header("HTTP/1.1 503 Service Temporarily Unavailable");
+header("Status: 503 Service Temporarily Unavailable");
+header("Retry-After: 3600");
+?>
+
 <!doctype html>
 <title>Site Maintenance</title>
 <style>
